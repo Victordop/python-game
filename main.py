55c3627ebacdefdeba84cfe63f32afe693733b65
vits <victordopr@gmail.com>
2026-05-25 06:52:47 -0300

    trouxe as classes do diagrama uml para o python

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init() #inicia pygame
 window = pygame.display.set_mode(size=(600,480)) #configura tela
 
-while True: # laço de repetição e condicional para mostrar a tela até fecha-la
+while True: # laço de repetição e condicional para mostrar a tela até fechar
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() #fecha janela
